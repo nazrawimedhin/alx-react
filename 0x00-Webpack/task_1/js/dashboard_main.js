006c46662941f175d905d8cc6566aede4dd56bd3
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-12 16:49:39 +0300

    task 1: using jquery

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,19 @@
+/* eslint-disable import/no-unresolved */
 const $ = require('jquery');
 const _ = require('lodash');
 
 let counter = 0;
 
-$("<p>Holberton Dashboard</p>").appendTo('body');
-$("<p>Dashboard data for the students</p>").appendTo('body');
-$("<button>Click here to get started</button>").appendTo('body');
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</button>').appendTo('body');
 $("<p id='count'></p>").appendTo('body');
-$("<p>Copyright - Holberton School</p>").appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
 
 function updateCounter() {
+  // eslint-disable-next-line no-plusplus
   counter++;
   $('#count').html(`${counter} clicks on the button`);
 }
 
-$("button").on("click", _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500));
