e7feb51008ae01e540d6c09b0b61d5f6f4791ea6
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-15 18:53:50 +0300

    task 0: created a function using JSX

---
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -0,0 +1,18 @@
+import logo from './Holberton-Logo.jpg'
+import './App.css'
+
+export default function App() {
+  return (
+  <div className="App">
+    <div className="App-header">
+        <img className="" src="Holberton-Logo.jpg"/>
+        <h1>School dashboard</h1>
+    </div>
+    <div className="App-body">
+      <p>Login to access the full dashboard</p>
+    </div>
+    <div className="App-footer">
+      <p>Copyright 2020 - holberton School</p>
+    </div>
+  </div>);
+}
