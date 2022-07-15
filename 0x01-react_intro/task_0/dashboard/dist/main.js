eb37b83cd911ccf41c262436500051b469269239
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-15 18:53:41 +0300

    webpack output

--- 0x01-react_intro/task_0/dashboard/dist/main.js
+++ 0x01-react_intro/task_0/dashboard/dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://dashboard/./src/index.js?");
+eval("throw new Error(\"Module parse failed: Unexpected token (6:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| export default function App() {\\n|   return (\\n>   <div className=\\\"App\\\">\\n|     <div className=\\\"App-header\\\">\\n|         <img className=\\\"\\\" src=\\\"Holberton-Logo.jpg\\\"/>\");\n\n//# sourceURL=webpack://dashboard/./src/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("\n\n//# sourceURL=webpack://dashboard/./src/index.js?");
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
