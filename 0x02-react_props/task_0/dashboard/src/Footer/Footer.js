f8807bada5ec56f0bb631e8b3a72ef35eca7e132
nazrawimedhin <nazrawimedhin1@gmail.com>
2022-07-20 22:36:20 +0300

    task_0: created basic components

---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import { getFooterCopy, getFullYear } from '../utils/utils';
+import './Footer.css';
+
+function Footer(){
+  return (
+    <div className="App-footer">
+      <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import logo from '../assets/holberton_logo.jpg';
+import './Header.css';
+
+function Header(){
+  return (
+    <div className="App-header">
+      <img src={logo} alt="logo" />
+      <h1>School dashboard</h1>
+    </div>
+  );
+}
+
+export default Header;
